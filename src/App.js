cccc4c31f0818b0069c7db6dd68435f6bdb173d9
Horlawale-ui <ajayiolawale40@ymail.com>
2021-12-05 23:16:26 +0100

    Change some Directory

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" exact element={<Home />} />
           <Route path="/services" element={<Services />} />
           <Route path="/products" element={<Products />} />
           <Route path="/signin" element={<SignIn />} />
