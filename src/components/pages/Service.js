717ed4399f1b87000a2e6df76f6a0c0d0ecaf724
Horlawale-ui <ajayiolawale40@ymail.com>
2021-12-09 01:19:25 +0100

    Added some responsiveness

--- src/components/pages/Service.js
+++ src/components/pages/Service.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Footer from "./Footer";
+import Video from "../../video/video.mp4";
 
 export default function Services() {
   return (
     <>
+      <video src={Video} muted loop autoPlay={true} controls=""></video>
       <h1
-        style={{
-          backgroundImage: `url(${
-            process.env.PUBLIC_URL + "/image/image-7.jpg"
-          })`,
-          backgroundRepeat: "no-repeat",
-        }}
+        // style={{
+        //   backgroundImage: `url(${
+        //     process.env.PUBLIC_URL + "/image/image-7.jpg"
+        //   })`,
+        //   backgroundRepeat: "no-repeat",
+        // }}
         className="services"
       >
         SERVICES
